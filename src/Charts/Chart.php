fd7e7fff4ad891232992d629cf0498e513aa3d97
Timothy Wood <codearachnid@gmail.com>
2025-04-14 22:21:13 -0400

    Create Chart.php

---
+++ src/Charts/Chart.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AlwaysCurious\Charts\Charts;
+
+use AlwaysCurious\Charts\Renderers\SvgRenderer;
+
+class Chart
+{
+    protected array $data = [];
+    protected array $options = [];
+
+    public function __construct(array $data = [], array $options = [])
+    {
+        $this->data($data);
+        $this->options($options);
+    }
+
+    public function data(array $data): static
+    {
+        $this->data = $data;
+        return $this;
+    }
+
+    public function options(array $options): static
+    {
+        $this->options = $options;
+        return $this;
+    }
+
+    public function render(): string
+    {
+        return (new SvgRenderer())->render($this->data, $this->options);
+    }
+}
